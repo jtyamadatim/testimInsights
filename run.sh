741f4aa89c9b28b54d6cb3b8682dcbf858beb6d7
Genesis Thomas <genesisthomasit@gmail.com>
2022-08-10 19:23:14 +0530

    update to run.sh

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ if [ "$PARAMS" = "" ]; then
 fi
 
 # removes any dangling images
-docker rmi $(docker images -f "dangling=true" -q)
+docker images -qf dangling=true | xargs -r docker rmi || true
 # Remove existing container if any
 docker container rm analyzer -f || true &&
     # pull image
@@ -34,11 +34,13 @@ docker container rm analyzer -f || true &&
     # build a local image with additional files if any
     docker build -q -t testim_analyzer . &&
     # Run container
-    docker run --memory-swap -1 -it --security-opt seccomp=unconfined --platform linux/amd64 -e API_KEY="${API_KEY}" -e PROJECT_ID="${PROJECT_ID}" --name analyzer -t testim_analyzer -n "${PROJECT_NAME}" -a "${PARAMS}" &&
+    docker run --memory-swap -1 --tty -e API_KEY="${API_KEY}" -e PROJECT_ID="${PROJECT_ID}" --name analyzer -t testim_analyzer -n "${PROJECT_NAME}" -a "${PARAMS}" &&
     #  Copy output folder from docker to current path
     docker cp analyzer:/app/src/python/output ${PWD} &&
     # Remove container
     docker container rm analyzer -f &&
-    # Remove dangling images if any
-    docker rmi $(docker images -f "dangling=true" -q) &&
+    # clean up dangling images, processes (if any)
+    docker images -qf dangling=true | xargs -r docker rmi || true &&
+    docker ps -aqf status=exited | xargs -r docker rm || true && 
+    docker system prune -f || true &&
     exit
